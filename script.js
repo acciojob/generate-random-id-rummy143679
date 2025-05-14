55087803da9a61c10d7df2b086f06b9f09995a62
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-14 20:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	let random = "";
+	for (let index = 0; index < l; index++) {
+		let num = Math.random()*str.length();
+		str += str[num];
+	}
+	return str;
 }
 
-// Do not change the code below.
+// Do not change the code below
 const l = prompt("Enter a number.");
 alert(makeid(l));
